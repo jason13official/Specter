77a191363a62851f625f21bf73037e8c40940812
jason13official <jason13official@gmail.com>
2025-10-19 20:11:30 -0400

    feat: healing player

--- common/src/main/java/com/cursee/specter/SpecterCommon.java
+++ common/src/main/java/com/cursee/specter/SpecterCommon.java
@@ -1,5 +1,6 @@
 package com.cursee.specter;
 
+import java.util.Random;
 import net.minecraft.resources.ResourceLocation;
 
 public class SpecterCommon {
@@ -9,5 +10,34 @@ public class SpecterCommon {
 
   public static ResourceLocation identifier(String path) {
     return ResourceLocation.tryBuild(Constants.MOD_ID, path);
+  }
+
+  public static void main(String[] args) {
+
+    Random random = new Random();
+
+    float lowestAchieved = Float.MAX_VALUE;
+    float highestAchieved = Float.MIN_VALUE;
+
+    for (int i = 0; i < 1000; i++) {
+
+
+      float randomFloat = random.nextFloat();
+
+      float calculated = (randomFloat * 2) - 1;
+
+      if (calculated > highestAchieved) {
+        highestAchieved = calculated;
+      }
+
+      if (calculated < lowestAchieved) {
+        lowestAchieved = calculated;
+      }
+
+      System.out.println("Calculated Value: " + String.valueOf(calculated));
+    }
+
+    System.out.println("Lowest Value Achieved: " + String.valueOf(lowestAchieved));
+    System.out.println("Highest Value Achieved: " + String.valueOf(highestAchieved));
   }
 }
--- common/src/main/java/com/cursee/specter/impl/common/entity/RawSpecter.java
+++ common/src/main/java/com/cursee/specter/impl/common/entity/RawSpecter.java
@@ -1,12 +1,17 @@
 package com.cursee.specter.impl.common.entity;
 
 import com.cursee.specter.impl.common.registry.ModEntities;
+import java.util.Random;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.Nullable;
 
 public class RawSpecter extends AbstractRawSpecter {
@@ -39,5 +44,34 @@ public class RawSpecter extends AbstractRawSpecter {
     super.tick();
 
     // new logic
+
+    LivingEntity owner = this.getOwner();
+    if (owner instanceof Player player) {
+
+      // oncer per second
+      if (this.tickCount % 20 == 0) {
+
+        // heal if close to player
+        if (this.distanceTo(player) < 4.0f) {
+          this.healOwner(player);
+        }
+      }
+    }
+  }
+
+  private void healOwner(Player player) {
+
+    if (player.getHealth() < (player.getMaxHealth() * 0.95f)) {
+
+      player.heal(2.0f);
+      player.level().playSound(null, this.blockPosition(), SoundEvents.ALLAY_THROW, SoundSource.AMBIENT, 0.8f, 0.8f);
+
+      Vec3 pos = this.position();
+      for (int i = 0; i < 4; i++) {
+        float random = player.getRandom().nextFloat();
+        float offset = (random * 2) - 1;
+        player.level().addParticle(ParticleTypes.HAPPY_VILLAGER, pos.x + (offset / 2), pos.y + (offset / 2), pos.z + (offset / 2), offset, offset, offset);
+      }
+    }
   }
 }
