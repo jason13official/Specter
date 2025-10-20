94c192405d9630123c6bca4b5db655cff56a9f06
jason13official <jason13official@gmail.com>
2025-10-20 14:55:59 -0400

    fix: rename entity mixin

--- common/src/main/java/com/cursee/specter/mixin/EntityPersistentDataMixin.java
+++ common/src/main/java/com/cursee/specter/mixin/EntityPersistentDataMixin.java
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Entity.class)
-public class LivingEntityPersistentDataMixin implements PersistentDataHolder {
+public class EntityPersistentDataMixin implements PersistentDataHolder {
 
   @Unique
   private CompoundTag specter$persistentData;
